076f8f30976034a80253c51e31fa940ff9233d1a
jetna <jetnasantos@gmail.com>
2020-06-10 19:52:16 -0300

    tarefa 014 incompleta

---
+++ Tarefa014funcionValidarCartao.js
@@ -0,0 +1,18 @@
+<link href="Tarefa014.html"></link>
+<script>
+                                        function validarCartao(numCartao) {
+                                                switch (numCartao) {
+                                                    case (numCartao.length!=16):
+                                                        alert("Cartão inválido. informe 16 números")
+                                                        break;
+                                                        case (valCartao.length!=4):
+                                                    alert("Cartão inválido. Informe 4 númeris")
+                                                        break;
+                                                        case (numSegCartao.length!=3):
+                                                    alert("Cartão inválido")
+                                                        break;
+                                                        default: validado=true
+                                                        break;
+                                                } //switch numCartao
+                                } //fim function validarCartao
+</script>
